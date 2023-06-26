94e9b147f769b8ab2f5fca4a3beeefddb79b976f
Alinka08 <alinazoldubaeva@gmail.com>
2023-06-26 00:02:47 -0500

    nav-bar

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../../componens/navBar/NavBar";
+import HeroImg from "../../componens/heroImg/HeroImg";
 
 function Home() {
   return (
     <div>
       <NavBar />
+      <HeroImg />
     </div>
   );
 }
